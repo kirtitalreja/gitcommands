c229b5e805b87dc0b25dd178c5621d9a3934b27a
kirtitalreja <99421552+kirtitalreja@users.noreply.github.com>
2022-04-29 10:48:00 +0530

    Add files via upload

---
+++ JavaMultiplication.java
@@ -0,0 +1,10 @@
+public class JavaMultiplication
+{  
+   public static void main(String args[])   
+ {  
+	int n1=Integer.parseInt(args[0]), n2=Integer.parseInt(args[1]), mul; 
+	System.out.println(n1 +" "+ n2); 
+	mul = n1 * n2;  
+	System.out.println("The multiplication of two numbers is: "+mul);  
+ }  
+}
